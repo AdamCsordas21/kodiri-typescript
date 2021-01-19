60e51734220c00360c2231cc3399f1eafc9124df
AdamCsordas21 <adamcsordas@hotmail.com>
2021-01-19 20:23:35 +0000

    Concatenate two words

--- lower-beginner/strings/index.spec.ts
+++ lower-beginner/strings/index.spec.ts
@@ -1,5 +1,6 @@
 import {
   concatenate,
+  concatenateTwo,
 } from './.'
 
 describe('concatenate function', () => {
@@ -9,10 +10,18 @@ describe('concatenate function', () => {
     // then
     expect(result).toEqual('AB')
   })
+  
   it('concatenates two lower characters', () => {
     // when
     const result = concatenate('a', 'b')
     // then
     expect(result).toEqual('ab')
+  })
+  
+  it('concatenates two words', () => {
+    // when
+    const result = concatenateTwo('Hello', 'world')
+    // then
+    expect(result).toEqual('Hello world')
   })
 })
