f4c34df44ad375bc92c2ff62b5a00b5291609e96
AdamCsordas21 <adamcsordas@hotmail.com>
2021-01-20 19:19:12 +0000

    Subtract two positive/negative numbers

--- lower-beginner/numbers/index.spec.ts
+++ lower-beginner/numbers/index.spec.ts
@@ -1,5 +1,6 @@
 import {
   add,
+  subtract,  
 } from '.'
 
 describe('add function', () => {
@@ -15,5 +16,21 @@ describe('add function', () => {
     const result = add(-3, -2)
     // then
     expect(result).toEqual(-5)
+  })
+})
+
+describe('subtract function', () => {
+  it('subtracts two positive number', () => {
+    // when
+    const result = subtract(5, 4)
+    // then
+    expect(result).toEqual(1)
+  })
+  
+  it('subtracts two negative numbers', () => {
+    // when
+    const result = subtract(-3, -2)
+    // then
+    expect(result).toEqual(-1)
   })
 })
--- lower-beginner/numbers/index.ts
+++ lower-beginner/numbers/index.ts
@@ -1,1 +1,3 @@
 export const add = (firstNum: number, secondNum: number): number => firstNum + secondNum
+
+export const subtract = (firstNum: number, secondNum: number) => firstNum - secondNum
