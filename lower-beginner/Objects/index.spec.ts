c6d20225d1a129820da0b9a8e8599dea4c5623c4
AdamCsordas21 <adamcsordas@hotmail.com>
2021-01-26 17:14:05 +0000

    Add getTotalScore function

--- lower-beginner/Objects/index.spec.ts
+++ lower-beginner/Objects/index.spec.ts
@@ -1,11 +1,12 @@
 import {
   getFirstName,
+  getTotalScore,
 } from '.'
 
 describe('getFirstName function', () => {
   [{
     it: 'Returns the first name of Paul Pogba',
-    player: { firstName: 'Paul', lastName: 'Pogba'},
+    player: { firstName: 'Paul', lastName: 'Pogba' },
     expected: 'Paul'
   }, {
     it: 'returns the first name of Marcus Rashford',
@@ -15,6 +16,25 @@ describe('getFirstName function', () => {
     it(scenario.it, () => {
       // when
       const result = getFirstName(scenario.player)
+      // then
+      expect(result).toEqual(scenario.expected)
+    })
+  })
+})
+
+describe('getTotalScore function', () => {
+  [{
+    it: 'Returns the total score for the first player',
+    player: { score1: 65, score2: '54' },
+    expected: 119
+  }, {
+    it: 'Returns the total score for the second player',
+    player: { score1: 56, score2: '42' },
+    expected: 98
+  }].forEach(scenario => {
+    it(scenario.it, () => {
+      // when
+      const result = getTotalScore(scenario.player)
       // then
       expect(result).toEqual(scenario.expected)
     })
