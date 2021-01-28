b9af7b25da71687d13c61d366e6dd1983699c940
AdamCsordas21 <adamcsordas@hotmail.com>
2021-01-28 19:50:30 +0000

    Add getDriverTitles function

--- lower-beginner/Optional-values/index.ts
+++ lower-beginner/Optional-values/index.ts
@@ -6,3 +6,10 @@ export const findFirstPositiveNum = (nums: number[]): number | string =>
 export const sum = (a: number, b: number, c?: number): number => c ? a + b + c : a + b
 
 export const isNumericList = (list: Array<number | string>): boolean => list.every(n => typeof n === 'number')
+
+export interface IDriver {
+  name: string,
+  titles?: number[]
+}
+
+export const getDriverTitles = (driver: IDriver): number[] | string => driver.titles || 'No titles yet'
