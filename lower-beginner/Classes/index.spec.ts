a4672318b0753d5b909d98322c81531ddd135006
AdamCsordas21 <adamcsordas@hotmail.com>
2021-02-01 16:25:31 +0000

    Add Greeter2 class/greet method

--- lower-beginner/Classes/index.spec.ts
+++ lower-beginner/Classes/index.spec.ts
@@ -16,12 +16,9 @@ describe('Greeter class', () => {
 describe('Greeter class', () => {
   describe('greet method', () => {
     it('returns a "hello world" message to a given user', () => {
-      // given
       const greeter = new Greeter2()
-      // when
       const helloBorisMessage = greeter.greet('Boris')
       const helloDonaldMessage = greeter.greet('Donald')
-      // then
       expect(helloBorisMessage).toEqual('Hello Boris')
       expect(helloDonaldMessage).toEqual('Hello Donald')
     })
