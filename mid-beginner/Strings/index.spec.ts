f7420624caa6fcc2dec69ecd987133502153eb86
AdamCsordas21 <adamcsordas@hotmail.com>
2021-02-03 20:36:40 +0000

    Mid-beginner: Add test case to concatenate function implementation

--- mid-beginner/Strings/index.spec.ts
+++ mid-beginner/Strings/index.spec.ts
@@ -3,8 +3,13 @@ import {
 } from '.'
 
 describe('concatenate function', () => {
-  it('concatenate two upper characters', () => {
+  it('concatenates two upper characters', () => {
     const result = concatenate('A', 'B')
     expect(result).toEqual('AB')
+  })
+
+  it('concatenates to lower characters', () => {
+    const result = concatenate('a', 'b')
+    expect(result).toEqual('ab')
   })
 })
