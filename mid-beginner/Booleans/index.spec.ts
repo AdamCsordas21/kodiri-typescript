a9d414cf47dd70a6b87819a96396964d4725bc2b
AdamCsordas21 <adamcsordas@hotmail.com>
2021-02-08 18:58:07 +0000

    Mid-beginner: Add second test case to negate function

--- mid-beginner/Booleans/index.spec.ts
+++ mid-beginner/Booleans/index.spec.ts
@@ -7,4 +7,8 @@ describe('negate function', () => {
     const result = negate(true)
     expect(result).toEqual(false)
   })
+  it('returns false when received false', () => {
+    const result = negate(false)
+    expect(result).toEqual(true)
+  })
 })
