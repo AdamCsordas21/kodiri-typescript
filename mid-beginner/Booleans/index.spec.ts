29e6cfaaf4448719579a67efffbfd61f31a6705c
AdamCsordas21 <adamcsordas@hotmail.com>
2021-02-08 19:31:44 +0000

    Mid-beginner: Add hasVictoryChances function

--- mid-beginner/Booleans/index.spec.ts
+++ mid-beginner/Booleans/index.spec.ts
@@ -1,6 +1,8 @@
 import {
   negate,
   amIAdult,
+  flagChecker,
+  hasVictoryChances,
 } from '.'
 
 describe('negate function', () => {
@@ -8,19 +10,54 @@ describe('negate function', () => {
     const result = negate(true)
     expect(result).toEqual(false)
   })
+  
   it('returns false when received false', () => {
     const result = negate(false)
     expect(result).toEqual(true)
   })
 })
 
-describe('naget function', () => {
+describe('negate function', () => {
   it('returns true when age is >= 18', () => {
     const result = amIAdult(19)
     expect(result).toEqual(true)
   })
+  
   it('returns false when age is < 18', () => {
     const result = amIAdult(16)
     expect(result).toEqual(false)
+  })
+})
+
+describe('flagChecker function', () => {
+  it('returns a message when a given argument is set to true', () => {
+    const result = flagChecker(true)
+    expect(result).toEqual('Flag set to true')
+  })
+  
+  it('returns a message when a given argument is set to false', () => {
+    const result = flagChecker(false)
+    expect(result).toEqual('Flag set to false')
+  })
+})
+
+describe('hasVictoryChances function', () => {
+  [{
+    title: 'can\'t win the race if both rider and bike fail',
+    isGoodRider: false, hasGoodBike: false, expected: false
+  }, {
+    title: 'can\'t win the race if rider is good but bike isn\'t',
+    isGoodRider: true, hasGoodBike: false, expected: false
+  }, {
+    title: 'can\'t win the race if the bike is good but the rider isn\'t',
+    isGoodRider: false, hasGoodBike: true, expected: false
+  }, {
+    title: 'can win the race if both rider and bike are good',
+    isGoodRider: true, hasGoodBike: true, expected: true
+  }].forEach(({ title, isGoodRider, hasGoodBike, expected }) => {
+    it(title, () => {
+      const result = hasVictoryChances(isGoodRider, hasGoodBike)
+      expect(result).toEqual(expected)
+    })
   })
 })
--- mid-beginner/Booleans/index.ts
+++ mid-beginner/Booleans/index.ts
@@ -1,3 +1,7 @@
 export const negate = (value: boolean): boolean => !value
 
-export const amIAdult = (age: number): boolean => age >= 18 ? true : false
+export const amIAdult = (age: number): boolean => age >= 18
+
+export const flagChecker = (flag: boolean): string => flag === true ? 'Flag set to true' : 'Flag set to false'
+
+export const hasVictoryChances = (isGoodRider: boolean, hasGoodBike: boolean): boolean => isGoodRider && hasGoodBike
