bac4770adceb5d5e801296f434ff07c9a21f14b4
AdamCsordas21 <adamcsordas@hotmail.com>
2021-02-08 19:15:09 +0000

    Mid-beginner: Add flagChecker function

--- mid-beginner/Booleans/index.spec.ts
+++ mid-beginner/Booleans/index.spec.ts
@@ -1,5 +1,6 @@
 import {
   negate,
+  amIAdult,
 } from '.'
 
 describe('negate function', () => {
@@ -10,5 +11,16 @@ describe('negate function', () => {
   it('returns false when received false', () => {
     const result = negate(false)
     expect(result).toEqual(true)
+  })
+})
+
+describe('naget function', () => {
+  it('returns true when age is >= 18', () => {
+    const result = amIAdult(19)
+    expect(result).toEqual(true)
+  })
+  it('returns false when age is < 18', () => {
+    const result = amIAdult(16)
+    expect(result).toEqual(false)
   })
 })
--- mid-beginner/Booleans/index.ts
+++ mid-beginner/Booleans/index.ts
@@ -1,1 +1,3 @@
 export const negate = (value: boolean): boolean => !value
+
+export const amIAdult = (age: number): boolean => age >= 18 ? true : false
