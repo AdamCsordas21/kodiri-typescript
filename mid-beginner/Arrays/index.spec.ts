44b551a38fd129763c3d5d63f2be0050e9aea0a6
AdamCsordas21 <adamcsordas@hotmail.com>
2021-02-09 19:07:31 +0000

    Mid-beginner - Arrays: Add getListSize function

--- mid-beginner/Arrays/index.spec.ts
+++ mid-beginner/Arrays/index.spec.ts
@@ -1,5 +1,6 @@
 import {
   getFirstElement,
+  getListSize,
 } from '.'
 
 describe('getFirstElement function', () => {
@@ -11,5 +12,14 @@ describe('getFirstElement function', () => {
   it('returns first character of a list', () => {
     const firstCharacter = getFirstElement(['Boris', 'Donald', 'Bill'])
     expect(firstCharacter).toEqual('Boris')
+  })
+})
+
+describe('getListSize function', () => {
+  it('returns the length of a list of elements', () => {
+    const player1Size = getListSize(['Boris', 55, 'boris@england.co.uk'])
+    const player2Size = getListSize(['Donald', 65, 2020, 32])
+    expect(player1Size).toEqual(3)
+    expect(player2Size).toEqual(4)
   })
 })
--- mid-beginner/Arrays/index.ts
+++ mid-beginner/Arrays/index.ts
@@ -1,1 +1,3 @@
 export const getFirstElement = (fruits: string[]): string => fruits[0]
+
+export const getListSize = (list: (string | number)[]): number => list.length
