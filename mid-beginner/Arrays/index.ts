6eb530629d1877ef559ed69dbbd3eacb90d6e4f2
AdamCsordas21 <adamcsordas@hotmail.com>
2021-02-10 18:52:14 +0000

    Mid-beginner - Arrays: Add getFirstOfSecondElement function

--- mid-beginner/Arrays/index.ts
+++ mid-beginner/Arrays/index.ts
@@ -3,3 +3,5 @@ export const getFirstElement = (fruits: string[]): string => fruits[0]
 export const getListSize = (list: (string | number)[]): number => list.length
 
 export const isListLong = (list: string[]): boolean => list.length >= 3
+
+export const getFirstOfSecondElement = (list: string[][]): string => list[1][0]
