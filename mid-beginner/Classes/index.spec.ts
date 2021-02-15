d2fa8f7d04a59e0977158c5c4f9d86f14b49d4e4
AdamCsordas21 <adamcsordas@hotmail.com>
2021-02-15 19:47:01 +0000

    Mid-beginner - Class: Add class Player2

--- mid-beginner/Classes/index.spec.ts
+++ mid-beginner/Classes/index.spec.ts
@@ -2,6 +2,7 @@ import {
   Greeter,
   Greeter2,
   Player,
+  Player2,
 } from '.'
 
 describe('Greeter class', () => {
@@ -32,6 +33,24 @@ describe('Player class', () => {
       const player = new Player('Boris')
       const userName = player.getUserName()
       expect(userName).toEqual('Boris')
+    })
+  })
+})
+
+describe('Player class', () => {
+  describe('getUserName method', () => {
+    it(`returns a "Boris Johnson" when these details are passed
+      to the constructor as individual arguments`, () => {
+      const player = new Player2('Boris', 'Johnson')
+      const userName = player.getUserName()
+      expect(userName).toEqual('Boris Johnson')
+    })
+    
+    it(`returns a "Donald Trump" when these details are passed
+      to the constructor as individual arguments`, () => {
+      const player = new Player2('Donald', 'Trump')
+      const userName = player.getUserName()
+      expect(userName).toEqual('Donald Trump')
     })
   })
 })
