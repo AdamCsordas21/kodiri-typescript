c8fbcded5bf370ee5978214d05deac6da8695a74
AdamCsordas21 <adamcsordas@hotmail.com>
2021-02-22 20:57:20 +0000

    Upper-beginner - Strings: Add test case to concatenate function (lower characters)

--- upper-beginner/Strings/index.spec.ts
+++ upper-beginner/Strings/index.spec.ts
@@ -6,5 +6,10 @@ describe('concatenate function', () => {
   it('concatenates two upper characters', () => {
     const result = concatenate('A', 'B')
     expect(result).toEqual('AB')
-  })  
+  })
+  
+  it('concatenates two lower characters', () => {
+    const result = concatenate('a', 'b')
+    expect(result).toEqual('ab')
+  })
 })
