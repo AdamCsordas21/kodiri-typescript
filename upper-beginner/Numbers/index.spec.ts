72584814fe83cfb98397a24557453e185740cfc2
AdamCsordas21 <adamcsordas@hotmail.com>
2021-02-24 22:28:40 +0000

    Uppre-beginner - Numbers: Add negative numbers test case to add function

--- upper-beginner/Numbers/index.spec.ts
+++ upper-beginner/Numbers/index.spec.ts
@@ -7,4 +7,9 @@ describe('add function', () => {
     const result = add(10, 7)
     expect(result).toEqual(17)
   })
+  
+  it('adds two negative numbers', () => {
+    const result = add(-21, -10)
+    expect(result).toEqual(-31)
+  })
 })
