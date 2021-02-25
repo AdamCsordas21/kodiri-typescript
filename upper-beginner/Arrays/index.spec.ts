9eacee93e803c9e061b97ca77e2c107358be0869
AdamCsordas21 <adamcsordas@hotmail.com>
2021-02-25 23:18:23 +0000

    Upper-beginner - Arrays: Add getListSize function

--- upper-beginner/Arrays/index.spec.ts
+++ upper-beginner/Arrays/index.spec.ts
@@ -1,5 +1,6 @@
 import {
   getFirstElement,
+  getListSize,
 } from '.'
 
 describe('getFirstElement function', () => {
@@ -11,5 +12,14 @@ describe('getFirstElement function', () => {
   it('returns the first element of a list', () => {
     const firstCharacter = getFirstElement(['Boris', 'Donald', 'Bill'])
     expect(firstCharacter).toEqual('Boris')
+  })
+})
+
+describe('getListSize', () => {
+  it('returns the length of a list of element', () => {
+    const player1Size = getListSize(['Boris', 55, 'boris@england.co.uk'])
+    const player2Size = getListSize(['Donald', 65, 2020, 2021])
+    expect(player1Size).toEqual(3)
+    expect(player2Size).toEqual(4)
   })
 })
--- upper-beginner/Arrays/index.ts
+++ upper-beginner/Arrays/index.ts
@@ -1,1 +1,3 @@
 export const getFirstElement = (list: string[]): string => list[0]
+
+export const getListSize = (list: Array<string | number>): number => list.length
