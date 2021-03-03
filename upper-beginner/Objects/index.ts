44c368986d40a19065ac51154eadf6f4ea2478e1
AdamCsordas21 <adamcsordas@hotmail.com>
2021-03-03 20:52:15 +0000

    Upper - beginner - Objects: Add getTotalScore function

--- upper-beginner/Objects/index.ts
+++ upper-beginner/Objects/index.ts
@@ -1,1 +1,4 @@
 export const getFirstName = (player: { firstName: string, lastName: string }): string => player.firstName
+
+export const getTotalScore = (player: { score1: number, score2: string }): number => 
+  player.score1 + parseInt(player.score2)
