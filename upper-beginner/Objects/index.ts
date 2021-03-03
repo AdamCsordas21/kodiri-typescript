cc895dca4c99afa61a9bbeeafbebc01563209bc2
AdamCsordas21 <adamcsordas@hotmail.com>
2021-03-03 21:04:30 +0000

    Upper - beginner - Objects: Add isLargeObject function

--- upper-beginner/Objects/index.ts
+++ upper-beginner/Objects/index.ts
@@ -2,3 +2,6 @@ export const getFirstName = (player: { firstName: string, lastName: string }): s
 
 export const getTotalScore = (player: { score1: number, score2: string }): number => 
   player.score1 + parseInt(player.score2)
+
+export const isGoodScore = (player: { name: string, age: number, score: number }): boolean =>
+  player.score >= 50 ? true : false
