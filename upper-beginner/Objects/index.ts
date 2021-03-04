34ac61e19f1c73d161061124d92b7f468dc61612
AdamCsordas21 <adamcsordas@hotmail.com>
2021-03-04 20:45:13 +0000

    Upper - beginner - Objects: Refactor canIEasilyGetAJob function

--- upper-beginner/Objects/index.ts
+++ upper-beginner/Objects/index.ts
@@ -9,11 +9,6 @@ export const isGoodScore = (player: { name: string, age: number, score: number }
 export const getUserDetails = (user: { name: string, age: number, location: string }): string =>
   `${user.name} aged ${user.age} currently is in ${user.location}.`
   
-export const canIEasilyGetAJob = (user: { yearsOfExperience: number, rightAttitude: boolean }): boolean => {
-  if (user.yearsOfExperience > 0 || user.rightAttitude === true) {
-    return true
-  } else {
-    return false
-  }
-}  
+export const canIEasilyGetAJob = (user: { yearsOfExperience: number, rightAttitude: boolean }): boolean =>
+  user.yearsOfExperience > 0 || user.rightAttitude
  
