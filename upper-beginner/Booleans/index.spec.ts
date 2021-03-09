0f57ccc908e37e785e15ae098f7c21c2cbb3bdd4
AdamCsordas21 <adamcsordas@hotmail.com>
2021-03-09 18:03:42 +0000

    Upper - beginner - Booleans: Add amIAnAdult function

--- upper-beginner/Booleans/index.spec.ts
+++ upper-beginner/Booleans/index.spec.ts
@@ -1,5 +1,6 @@
 import {
   negate,
+  amIAnAdult,
 } from '.'
 
 describe('negate function', () => {
@@ -11,5 +12,17 @@ describe('negate function', () => {
   it('returns false when received false', () => {
     const result = negate(false)
     expect(result).toEqual(true)
+  })
+})
+
+describe('negate function', () => {
+  it('returns true when age is >= 18', () => {
+    const result = amIAnAdult(19)
+    expect(result).toEqual(true)
+  })
+  
+  it('returns false when age is < 18', () => {
+    const result = amIAnAdult(16)
+    expect(result).toEqual(false)
   })
 })
--- upper-beginner/Booleans/index.ts
+++ upper-beginner/Booleans/index.ts
@@ -1,1 +1,9 @@
 export const negate = (value: boolean): boolean => !value
+
+export const amIAnAdult = (age: number): boolean => {
+  if (age >= 18) {
+    return true
+  } else {
+    return false
+  }
+}
