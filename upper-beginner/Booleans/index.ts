f3e13be7c00523f9c9edd86b74e052c072434542
AdamCsordas21 <adamcsordas@hotmail.com>
2021-03-09 17:57:10 +0000

    Upper - beginner - Booleans: Refactor negate function

--- upper-beginner/Booleans/index.ts
+++ upper-beginner/Booleans/index.ts
@@ -1,8 +1,3 @@
 export const negate = (value: boolean): boolean => {
-  if (value === true) {
-    return false
-  }
-  if(value === false) {
-    return true
-  }
+  return value !== true ? true : false
 }
