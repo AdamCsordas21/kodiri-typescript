7d0080aa44c9ed6fc8645ec5011f69dc1a0deb57
AdamCsordas21 <adamcsordas@hotmail.com>
2021-03-11 20:33:02 +0000

    Upper - beginner - Booleans: Add hasVictoryChances function

--- upper-beginner/Booleans/index.spec.ts
+++ upper-beginner/Booleans/index.spec.ts
@@ -2,6 +2,7 @@ import {
   negate,
   amIAnAdult,
   flagChecker,
+  hasVictoryChances,
 } from '.'
 
 describe('negate function', () => {
@@ -37,5 +38,26 @@ describe('flagChecker function', () => {
   it('returns a message when a given arguments is set to false', () => {
     const result = flagChecker(false)
     expect(result).toEqual('Flag set to false')
+  })
+})
+
+describe('hasVictoryChances function', () => {
+  [{
+    title: 'cannot win the race if both rider and bike fail',
+    isGoodRider: false, hasGoodBike: false, expected: false
+  }, {
+    title: 'cannot win the race if rider is good but bike isn\'t',
+    isGoodRider: true, hasGoodBike: false, expected: false
+  }, {
+    title: 'cannot win the race if the bike is good but the rider isn\'t',
+    isGoodRider: false, hasGoodBike: true, expected: false
+  }, {
+    title: 'can win the race if both rider and bike are good',
+    isGoodRider: true, hasGoodBike: true, expected: true
+  }].forEach(({ title, isGoodRider, hasGoodBike, expected }) => {
+    it(title, () => {
+      const result = hasVictoryChances(isGoodRider, hasGoodBike)
+      expect(result).toEqual(expected)
+    })
   })
 })
