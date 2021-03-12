b2198f77a43a9f9a11d424dca944e4769a5b9ccd
AdamCsordas21 <adamcsordas@hotmail.com>
2021-03-12 21:03:58 +0000

    Upper - beginner - Optional-values: Add findFirstPositiveNum function

--- upper-beginner/Optional-values/index.ts
+++ upper-beginner/Optional-values/index.ts
@@ -1,1 +1,5 @@
 export const checkEmail = (name: string, email?: string): string => email ? `Email provided` : `Email not provided`
+
+export const findFirstPositiveNum = (list: number[]): number | string => {
+  return list.find(n => n > 0) || `No positive number available`
+}
