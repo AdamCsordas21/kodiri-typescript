10c176ce8ba45c26c4fb32878b917d260aa98f40
AdamCsordas21 <adamcsordas@hotmail.com>
2021-03-12 21:10:37 +0000

    Upper - beginner - Optional-values: Add sum function

--- upper-beginner/Optional-values/index.ts
+++ upper-beginner/Optional-values/index.ts
@@ -3,3 +3,5 @@ export const checkEmail = (name: string, email?: string): string =>
 
 export const findFirstPositiveNum = (list: number[]): number | string =>
   list.find(n => n > 0) || `No positive number available`
+
+export const sum = (num1: number, num2: number, num3?: number) => num1 + num2
