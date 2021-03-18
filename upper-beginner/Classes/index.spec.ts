c554cc37f2acc410162f8f3cc28417703b5e1537
AdamCsordas21 <adamcsordas@hotmail.com>
2021-03-18 18:57:44 +0000

    Upper - beginner - Classes: Add Greeter class

---
+++ upper-beginner/Classes/index.spec.ts
@@ -0,0 +1,13 @@
+import {
+  Greeter,
+} from '.'
+
+describe('Greeter class', () => {
+  describe('greet method', () => {
+    it('returns a "hello world" message', () => {
+      const greeter = new Greeter()
+      const result = greeter.greet()
+      expect(result).toEqual('Hello World!')
+    })
+  })
+})
