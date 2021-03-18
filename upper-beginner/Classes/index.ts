66db86957903bb835c1110b591b54d3c4bfd8e24
AdamCsordas21 <adamcsordas@hotmail.com>
2021-03-18 19:18:35 +0000

    Upper - beginner - Classes: Add Player class

--- upper-beginner/Classes/index.ts
+++ upper-beginner/Classes/index.ts
@@ -9,3 +9,13 @@ export class Greeter2 {
     return `Hello ${name}`
   }
 }
+
+export class Player {
+  private name: string
+  constructor(name: string) {
+    this.name = name
+  }
+  getUserName(): string {
+    return `${this.name}`
+  }
+}
