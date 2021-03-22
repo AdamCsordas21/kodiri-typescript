d00d743c0bf90876ba120de7ecd19ce67a3e01ac
AdamCsordas21 <adamcsordas@hotmail.com>
2021-03-22 19:32:52 +0000

    Upper - beginner - Interfaces: Add test case getFirstName function with interface Politician

--- upper-beginner/Interfaces/index.spec.ts
+++ upper-beginner/Interfaces/index.spec.ts
@@ -7,4 +7,9 @@ describe('getFirstName function', () => {
     const firstName = getFirstName({ firstName: 'Boris', lastName: 'Johnson' })
     expect(firstName).toEqual('Boris')
   })
+  
+  it('returns Donald when Trump last name is provided', () => {
+    const firstName = getFirstName({ firstName: 'Donald', lastName: 'Trump' })
+    expect(firstName).toEqual('Donald')
+  })
 })
