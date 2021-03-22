5532c9e64032eefc54d51a6e1c1683b8ff5b1424
AdamCsordas21 <adamcsordas@hotmail.com>
2021-03-22 19:47:13 +0000

    Upper - beginner - Interfaces: Add getFirstName2 function with interface Politician2

--- upper-beginner/Interfaces/index.spec.ts
+++ upper-beginner/Interfaces/index.spec.ts
@@ -1,5 +1,6 @@
 import {
   getFirstName,
+  getFirstName2,
 } from '.'
 
 describe('getFirstName function', () => {
@@ -7,9 +8,16 @@ describe('getFirstName function', () => {
     const firstName = getFirstName({ firstName: 'Boris', lastName: 'Johnson' })
     expect(firstName).toEqual('Boris')
   })
-  
+
   it('returns Donald when Trump last name is provided', () => {
     const firstName = getFirstName({ firstName: 'Donald', lastName: 'Trump' })
     expect(firstName).toEqual('Donald')
+  })
+})
+
+describe('getFirstName2 function', () => {
+  it('returns Boris when Johonson last name is provided', () => {
+    const firstName = getFirstName2({ name: { firstName: 'Boris', lastName: 'Johnson' }, age: 55 })
+    expect(firstName).toEqual('Boris')
   })
 })
--- upper-beginner/Interfaces/index.ts
+++ upper-beginner/Interfaces/index.ts
@@ -4,3 +4,15 @@ interface Politician {
 }
 
 export const getFirstName = (person: Politician): string => person.firstName
+
+interface Politician2 {
+  name: {
+    firstName: string
+    lastName: string
+  }
+  age: number
+}
+
+export const getFirstName2 = (person: Politician2) => {
+  return person.name.firstName
+}
